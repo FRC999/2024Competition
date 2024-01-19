75e6208ae06e31752aee1cc95fc4ee4741026b72
SamH434 <77312898+SamH434@users.noreply.github.com>
2024-01-18 20:11:14 -0500

    Finished IMUSubysystem, continuing to Swerve

--- src/main/java/frc/robot/subsystems/IMUSubsystem.java
+++ src/main/java/frc/robot/subsystems/IMUSubsystem.java
@@ -1,22 +1,38 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.WPI_Pigeon2;
+import com.ctre.phoenix6.hardware.Pigeon2;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
+@SuppressWarnings({ "deprecation", "removal" })
+
+/*
+ * The IMU object should be instantiated from this class. The class has a
+ * selector that will
+ * create additional hardware-specific objects. However, other subsystems and
+ * commands will
+ * use this passthrough class, which exposes "standard" IMU methods that should
+ * be implemented
+ * by all hardware-specific classes. That way the non-IMU code does not need to
+ * know which IMU
+ * is actually used.
+ * 
+ * Since IMU represents a separate hardware component, it's defined as a
+ * subsystem
+ * (via "extends SubsystemBase"). It means that you can add "periodic" code to
+ * it.
+ * We do not currently need that. However, when used in a competition, you might
+ * find it necessary to do so.
+ */
+
 public class IMUSubsystem extends SubsystemBase {
 
-  private static WPI_Pigeon2 imu;
-
-  private double trajectoryAdjustmentIMU; // This is the value we need to adjust the IMU by after Trajectory
-                                          // is completed
+  private static WPI_Pigeon2 imu; // We will use downcasting to set this - it will point to methods either in NavX
+  // or Pigeon subsystems
 
   /**
    * Creates a new IMUSubsystem.
@@ -33,81 +49,106 @@ public class IMUSubsystem extends SubsystemBase {
    */
   public IMUSubsystem() {
 
-    /*
-     * Depending on the IMU type specified in Constants, the variable "imu" will
-     * point to the instance
-     * of the class specific to the hardware you have, e.g. Pigeon2 or NavX
-     * All such implementation classes must have public methods specified by the
-     * IMUInterface
-     */
-    
-      imu = new WPI_Pigeon2(Constants.IMUConstants.PIGEON2_CHANNEL);
-    
-    }
+    imu = new WPI_Pigeon2(Constants.IMUConstants.PIGEON2_CHANNEL);
 
-    imu.zeroGyroBiasNow(); // TODO : At the start of game, robot must be pointed towards opposite team's side
-                   // (This is our zero value). We do not know which side red/blue is on
+    //I am calling this because for some reason zeroYaw gives error for 
+    //  "The method zeroYaw() is undefined for the type WPI_Pigeon2"
+    //imu.zeroYaw();
+    imu.setYaw(0);
 
   }
 
   /**
-   * Note that all IMU methods that take or return values should do so in SI units.
+   * Note that all IMU methods that take or return values should do so in SI
+   * units.
    */
 
   public double getPitch() {
-    return imu.getPitch();
+    // double[] ypr = new double[3];
+    // pigeon2.getYawPitchRoll(ypr);
+    // return ypr[1];
+
+    // Front UP - positive Pitch
+    return -imu.getPitch();
   }
 
+  /**
+   * Gets the roll of the robot (Y axis rotation) (roll is the leaning around the
+   * axis that goes straight forward)
+   * 
+   * @return
+   */
   public double getRoll() {
+    // double[] ypr = new double[3];
+    // pigeon2.getYawPitchRoll(ypr);
+    // return ypr[2];
+
+    // Left UP - positive Roll
     return imu.getRoll();
   }
 
+  /**
+   * Gets the yaw of the robot (Z axis rotation) (yaw is the direction that the
+   * robot is facing around an axis that shoots straight up)
+   * 
+   * @return
+   */
   public double getYaw() {
-    return imu.getYaw();
+    // double[] ypr = new double[3];
+    // pigeon2.getYawPitchRoll(ypr);
+    // System.out.println(ypr[0]);
+    // return ypr[0];
+
+    return imu.getYaw(); // With Pigeon2 this method returns values in degrees
+  }
+
+  public Rotation2d getYawRotation2d() {
+    return Rotation2d.fromDegrees(getYaw());
+  }
+
+  /**
+   * Zeroes the yaw of the robot
+   * 
+   * @return The previous yaw
+   */
+  public double zeroYaw() {
+    double previousYaw = getYaw();
+    imu.setYaw(0);
+    return previousYaw;
+  }
+
+  public double setYaw(double y) {
+    double previousYaw = getYaw();
+    imu.setYaw(y);
+    return previousYaw;
   }
 
   public Rotation2d getRotation2d() {
     return imu.getRotation2d();
   }
 
-  public void zeroYaw() {
+  /** Zeroes the heading of the robot. */
+  public void zeroHeading() {
     imu.setYaw(0);
-  }
-
-  public void setYaw(double y) {
-    imu.setYaw(y);
+    System.out.println("Yaw and Fused Heading set");
   }
 
   /**
-   * This method is used when we want to "snap" the chassis to a trajectory start, meaning
-   * assuming that the robot is at the starting point of the trajectory.
-   * Here we remember starting Yaw before trajectory so it can be restored
-   * back after trajectory
-   * @param y - starting Yaw of the trajectory
-   * @return - old value of the Yaw (we do not currently use it)
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180 in Rotation2d format
    */
-  public void setYawForTrajectory(double y) {
-    trajectoryAdjustmentIMU = RobotContainer.imuSubsystem.getYaw() - y;
-    imu.setYaw(y);
+  public Rotation2d getHeading() {
+    return imu.getRotation2d();
   }
 
   /**
-   * Once the trajectory is done, we want to readjust the Yaw considering the value that we "remember", so
-   * the field-centric drive axis will not change. That may allow one to drive automated trajectories in teleop
-   * without losing the Yaw direction.
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
    */
-  public void restoreYawAfterTrajectory() {
-    imu.setYaw(RobotContainer.imuSubsystem.getYaw() + trajectoryAdjustmentIMU);
-  }
-
-/*
   public double getTurnRate() {
-    return imu.getTurnRate();
-  }
-*/
-
-  public double getCompassHeading() {
-    return imu.getCompassHeading();
+    return -imu.getRate();
   }
 
 }
