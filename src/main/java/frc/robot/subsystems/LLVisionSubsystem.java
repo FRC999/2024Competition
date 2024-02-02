a59cd1edbba3d73b075c4c28321a7148d150d56d
Alex Tsekhansky <atsekhan@gmail.com>
2024-02-01 23:09:37 -0500

    add camera to chassis transformation

--- src/main/java/frc/robot/subsystems/LLVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/LLVisionSubsystem.java
@@ -6,7 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SwerveChassis;
 import frc.robot.Constants.VisionConstants.LimeLightConstants;
 import frc.robot.lib.LimelightHelpers;
 import frc.robot.lib.VisionHelpers;
@@ -14,6 +16,8 @@ import frc.robot.lib.VisionHelpers;
 public class LLVisionSubsystem extends SubsystemBase implements VisionHelpers {
 
   private Pose2d nullPose = new Pose2d(0,0,new Rotation2d(0));
+  // position of the LL relative to the center of the chassis; the X value may need to be remeasured if used to correct odometry
+  private Transform2d cameraPoseInRobotSpace = new Transform2d(-SwerveChassis.CHASSIS_OUTER_DIMENSIONS_X/2.0,0,new Rotation2d(Math.PI/2));
 
   /** Creates a new LLVisionSubsystem. 
    * Vision Subsystem based on LimeLight.
@@ -24,7 +28,8 @@ public class LLVisionSubsystem extends SubsystemBase implements VisionHelpers {
   public Pose2d getRobotFieldPoseLL() {
     if (LimelightHelpers.getTV(LimeLightConstants.LLAprilTagName)) { // LL target visible - meaning - see an Apriltag
       //System.out.println(LimelightHelpers.getBotPose2d_wpiBlue(LimeLightConstants.LLAprilTagName));
-      return LimelightHelpers.getBotPose2d_wpiBlue(LimeLightConstants.LLAprilTagName); //TODO: Check if the coordinates need to be translated to 0,0 of the blue lower corner
+      // We need to transform the 
+      return LimelightHelpers.getBotPose2d_wpiBlue(LimeLightConstants.LLAprilTagName).transformBy(cameraPoseInRobotSpace); //TODO: Check if the coordinates need to be translated to 0,0 of the blue lower corner
       // return LimelightHelpers.getBotPose2d_wpiBlue(LimeLightConstants.LLAprilTagName).relativeTo(LimeLightConstants.centerFieldPose); // check if this returns the right pose from 0,0
     } else {
       return null;
