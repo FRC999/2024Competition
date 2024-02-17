130b74e960d540a756d8f4a1ce7a5db858b96150
atsekhan <atsekhan@gmail.com>
2024-02-17 18:34:10 -0500

    Driver camera working on dStation dashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -18,6 +19,8 @@ import frc.robot.commands.AutonomousCommandPlaceholder;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
+
+
   private RobotContainer m_robotContainer;
 
   /**
@@ -29,6 +32,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    CameraServer.startAutomaticCapture();
   }
 
   /**
