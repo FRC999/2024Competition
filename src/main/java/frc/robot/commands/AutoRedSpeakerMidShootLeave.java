d91f00785f1b35905dfaaf60e4c980400c2f58cc
atsekhan <atsekhan@gmail.com>
2024-02-28 21:12:06 -0500

    Shoot & leave commands for Red+Blue Alliance

--- src/main/java/frc/robot/commands/AutoRedSpeakerMidShootLeave.java
+++ src/main/java/frc/robot/commands/AutoRedSpeakerMidShootLeave.java
@@ -4,34 +4,30 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants;
-import frc.robot.Constants.AutoConstants.BlueSpeakerBottomSideConstants;
-import frc.robot.Constants.AutoConstants.BlueSpeakerTopSideConstants;
-import frc.robot.Constants.AutoConstants.autoPoses;
 import frc.robot.RobotContainer;
+import frc.robot.Constants.AutoConstants.autoPoses;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoBlueSpeakerTopShootLeave extends SequentialCommandGroup {
-  /** Creates a new AutoBlueSpeakerTopShootLeave. */
-  public AutoBlueSpeakerTopShootLeave() {
+public class AutoRedSpeakerMidShootLeave extends SequentialCommandGroup {
+  /** Creates a new AutoRedSpeakerMidShootLeave. */
+  public AutoRedSpeakerMidShootLeave() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new InstantCommand( () -> RobotContainer.imuSubsystem.setYaw(
-          autoPoses.BLUE_SPEAKER_HIGHER.getPose().getRotation().getDegrees())), // set yaw to the one in the initial pose
+       new InstantCommand( () -> RobotContainer.imuSubsystem.setYaw(
+        autoPoses.RED_SPEAKER_MID.getPose().getRotation().getDegrees())), // set yaw to the one in the initial pose
       new WaitCommand(20).deadlineWith(
         new ShootingGPM0Sequence(0)),   // shoot
+      new WaitCommand(5.0), //wait 5 seconds to give time for other ; ADJUST BASED ON HOW LONG IT TAKES TO LEAVE COMMUNITY
       new AutonomousTrajectory2Poses(
-        autoPoses.BLUE_SPEAKER_HIGHER.getPose(),
-        autoPoses.BLUE_LOWER_POS_OUT.getPose()
-      )  //this will be our trajectory where we go from (0.46, 4.722478) to (3.25, 0.8) to leave community
+        autoPoses.RED_SPEAKER_MID.getPose(),
+        autoPoses.RED_MID_POS_OUT.getPose()
+      )  //this will be our trajectory where we go from (13.789, 5.514) to (11.453, 5.540) to leave community
     );
   }
 }
