48a4ec70781c00ea585062dd6e00e7f9c618f8dc
Alex Tsekhansky <atsekhan@gmail.com>
2024-03-06 00:56:31 -0500

    attempt at pose rotation around different origin

--- src/main/java/frc/robot/lib/TrajectoryHelpers.java
+++ src/main/java/frc/robot/lib/TrajectoryHelpers.java
@@ -1,10 +1,21 @@
 package frc.robot.lib;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 
 public class TrajectoryHelpers {
+
+    /**
+     * This method will adjust the endPose based on the angle.
+     * For now it will siply turn it to the specified angle using the startPose as origin.
+     * This will likely be sufficient for small trajectory deviations during auto-driving to pickup notes
+     */
     public static Pose2d correctEndingPoseBasedOnNoteLocation(Pose2d startPose, Pose2d endPose, double angle) {
-        return endPose;
+        Pose2d relativePoseTurned = endPose.relativeTo(startPose).rotateBy(Rotation2d.fromDegrees(angle));
+        Transform2d t = relativePoseTurned.minus(new Pose2d()); // converts relative pose to a transformation
+        return startPose.transformBy(t);
+        
     }
 
 }
