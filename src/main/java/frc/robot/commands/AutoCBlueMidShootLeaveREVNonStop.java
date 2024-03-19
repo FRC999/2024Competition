74c510388260e1fd36d10c388d17f60940f7b899
Tristan Harrison <firstroboticstristan@gmail.com>
2024-03-18 21:04:50 -0400

    REV nonstop Autos Blue and Red

---
+++ src/main/java/frc/robot/commands/AutoCBlueMidShootLeaveREVNonStop.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.AutoConstants.autoPoses;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoCBlueMidShootLeaveREVNonStop extends SequentialCommandGroup {
+  /** Creates a new AutoBlueSpeakerMidShootLeave. */
+  
+  public AutoCBlueMidShootLeaveREVNonStop() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new InstantCommand( () -> RobotContainer.imuSubsystem.setYaw(
+          autoPoses.BLUE_SPEAKER_MID.getPose().getRotation().getDegrees())), // set yaw to the one in the initial pose
+      new WaitCommand(10).deadlineWith(
+        (new ShootingGPM0Sequence(0))
+          .andThen(new ShooterStop()) // stop shooter
+          .andThen(new IntakeStop())) // stop intake
+        ,   // shoot
+      new WaitCommand(5.0), //wait 5 seconds to give time for other
+      new AutonomousTrajectory2Poses(
+        autoPoses.BLUE_SPEAKER_MID.getPose(),
+        autoPoses.BLUE_MID_POS_OUT.getPose()
+      )  //this will be our trajectory where we go from (6,) to (3.25, 0.8) to leave community
+    );
+  }
+}
---
+++ src/main/java/frc/robot/commands/AutoCRedLower2REVNonStop.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.AutoConstants.autoPoses;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoCRedLower2REVNonStop extends SequentialCommandGroup {
+  /** Creates a new AutoREDBottom3Notes. */
+  public AutoCRedLower2REVNonStop() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+
+        // set yaw
+        new InstantCommand(() -> RobotContainer.imuSubsystem.setYaw(
+            autoPoses.RED_SPEAKER_LOWER.getPose().getRotation().getDegrees())), // set yaw to the one in the initial pose
+
+        // shoot
+        new ShootingGPM0Sequence(0) // shoot
+            .andThen(new IntakeStop()), // stop intake
+
+        // arm down
+        new ArmDownToIntake(),
+
+        // drive to 2nd note pickup point
+        new AutonomousTrajectory2Poses( // drive to 1st note pickup
+            autoPoses.RED_SPEAKER_LOWER.getPose(),
+            autoPoses.RED_LOWER_RING_TAKE_START.getPose()),
+
+        // pickup note
+        new AutonomousTrajectory2Poses( // drive and run intake to pickup 1st note
+            autoPoses.RED_LOWER_RING_TAKE_START.getPose(),
+            autoPoses.RED_LOWER_RING_TAKE_END.getPose())
+            .deadlineWith(
+                new IntakeGrabNote()),
+
+        // stop intake
+        new IntakeStop(), // in case we did not grab the note
+        new ControllerRumbleStop(),
+
+        // go back to speaker
+        new AutonomousTrajectory2Poses( // drive to original position and turn arm to angle preemptively to reduce
+                                        // shooting cycle
+            autoPoses.RED_LOWER_RING_TAKE_END.getPose(),
+            autoPoses.RED_SPEAKER_LOWER.getPose())
+            .deadlineWith(
+                new ArmTurnToAngle(() -> RobotContainer.gpmHelpers.getGPM0Angle(0))),
+
+        // shoot
+        new ShootingGPM0Sequence(0),
+
+        // cleanup
+        new ShooterStop(), // stop shooter
+        new IntakeStop(), // stop intake
+        new ControllerRumbleStop(),
+
+        // drive out
+        new AutonomousTrajectory2Poses( // drive to 2nd pickup point
+            autoPoses.RED_SPEAKER_LOWER.getPose(),
+            autoPoses.RED_LOWER_POS_OUT.getPose()),
+        new PrintCommand("Auto DONE"));
+  }
+}
